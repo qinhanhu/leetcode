1c31ed78e2d98fc6fd4726f5f9ecc4d6ff5e4f36
QinhanHu <huqinhan1997@gmail.com>
2022-06-17 18:18:19 -0400

    l122 dp

--- l122_best_time_buy_sell_stock.py
+++ l122_best_time_buy_sell_stock.py
@@ -8,3 +8,23 @@ class Solution:
             if profit > 0:
                 res += profit
         return res
+
+
+# DP
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        dp[i][0]: the max profit you can get when u hold one stock within 0-ith days.
+        dp[i][1]: the max profit ~ when u dont hold any stock ~.
+        """
+
+        dp = []
+        for _ in range(len(prices)):
+            dp.append([0, 0])
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+            dp[i][1] = max(dp[i-1][1], prices[i] + dp[i-1][0])
+        return max(dp[-1][0], dp[-1][1])
