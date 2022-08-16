271e7db872300a8a19aa649d0959f400ff17eb0c
QinhanHu <huqinhan1997@gmail.com>
2022-08-16 14:09:54 -0400

    l785

--- l785.py
+++ l785.py
@@ -1,29 +1,29 @@
 # DFS
-# class Solution:
-#     def isBipartite(self, graph: List[List[int]]) -> bool:
-#         flag = True
-#         visited = [False] * len(graph)
-#         color = [0] * len(graph)
-#         def dfs(graph, node):
-#             nonlocal flag
-#             if flag == False:
-#                 return
-#             visited[node] = True
-#             for neighbor in graph[node]:
-#                 if not visited[neighbor]:
-#                     if color[node] == 0:
-#                         color[neighbor] = 1
-#                     else:
-#                         color[neighbor] = 0
-#                     dfs(graph, neighbor)
-#                 else:
-#                     if color[neighbor] == color[node]:
-#                         flag = False
-#                         return
-#         for node in range(len(graph)):
-#             if not visited[node]:
-#                 dfs(graph, node)
-#         return flag
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        flag = True
+        visited = [False] * len(graph)
+        color = [0] * len(graph)
+        def dfs(graph, node):
+            nonlocal flag
+            if flag == False:
+                return
+            visited[node] = True
+            for neighbor in graph[node]:
+                if not visited[neighbor]:
+                    if color[node] == 0:
+                        color[neighbor] = 1
+                    else:
+                        color[neighbor] = 0
+                    dfs(graph, neighbor)
+                else:
+                    if color[neighbor] == color[node]:
+                        flag = False
+                        return
+        for node in range(len(graph)):
+            if not visited[node]:
+                dfs(graph, node)
+        return flag
                     
 
 # BFS
@@ -56,20 +56,6 @@ class Solution:
             if not visited[node]:
                 bfs(graph, node)
         return flag
-
-    def A(self):
-        b = 1
-        b = self.B(b)
-        print(b)
-
-    def B(self, val):
-        val = 0
-        return val
-
-
-s = Solution()
-s.A()
-
 
                         
 
